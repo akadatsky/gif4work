145c28457c1428d8b10c8c6a82866eece2c6c539
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2020-12-15 13:17:57 +0200

    Add searchView

--- app/src/main/java/com/akadatsky/gif4work/MainActivity.kt
+++ app/src/main/java/com/akadatsky/gif4work/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.akadatsky.gif4work
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        searchView.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                performSearch(searchView.text.toString())
+            }
+            true
+        }
     }
+
+    private fun performSearch(searchText: String) {
+        toast(searchText)
+    }
+
 }
